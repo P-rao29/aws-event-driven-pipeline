a3ab786125dd0d7a1bfe7e280f62286afb0a30de
p_rao29 <128228904+P-rao29@users.noreply.github.com>
2025-12-30 14:06:48 +0530

    Add AWS Lambda functions for processor and report

---
+++ terraform/lambda.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "processor" {
+  function_name = "${var.project_name}-processor"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "data_processor.lambda_handler"
+  runtime       = "python3.10"
+
+  filename         = "../lambda/data_processor.zip"
+  source_code_hash = filebase64sha256("../lambda/data_processor.zip")
+}
+
+resource "aws_lambda_function" "report" {
+  function_name = "${var.project_name}-daily-report"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "daily_report.lambda_handler"
+  runtime       = "python3.10"
+
+  filename         = "../lambda/daily_report.zip"
+  source_code_hash = filebase64sha256("../lambda/daily_report.zip")
+}
